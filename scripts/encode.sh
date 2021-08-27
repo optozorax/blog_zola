edac831ba1e493db80835fb8f6969b9ac55df40e
ilya sheprut <optozorax@gmail.com>
2021-08-27 12:19:59 +0600

    many improvements * fix audio in moonlander article * fix images inside container

--- scripts/encode.sh
+++ scripts/encode.sh
@@ -8,4 +8,7 @@ extension="${filename##*.}"
 filename="${filename%.*}"
 path="${1%/*}"
 
-ffmpeg -y -i $1 -c:v libx264 -pix_fmt yuv420p -profile:v baseline -level 3.0 -crf ${2:-35} -preset veryslow -vf "scale=ceil(iw*min(1\,1280/iw)/2)*2:ceil(ow/dar/2)*2" -an -movflags +faststart -threads 0 ${path}/${filename}_web.mp4
+not_audio="-an"
+audio="-c:a aac -strict experimental"
+
+ffmpeg -y -i $1 -c:v libx264 -pix_fmt yuv420p -profile:v baseline -level 3.0 -crf ${2:-35} -preset veryslow -vf "scale=ceil(iw*min(1\,1280/iw)/2)*2:ceil(ow/dar/2)*2" ${audio} -movflags +faststart -threads 0 ${path}/${filename}_web.mp4
